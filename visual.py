2c8cc667292da09aa7370c622be56f20df234f0e
ssyuen <samuel.s.yuen@gmail.com>
2020-02-07 20:31:21 -0500

    latest_perf isolated

--- visual.py
+++ visual.py
@@ -4,10 +4,16 @@ import pandas as pd
 
 portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'BLK~25|AAPL~25|IXN~25|MALOX~25'})
 
+data = portfolioAnalysisRequest.json()
+
 
 f = open('out.json','a')
-f.write(json.dumps(portfolioAnalysisRequest.json()))
+f.write(json.dumps(data))
+
+print(data['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['returns']['latestPerf'])
 
 
+f = open('latest_perf.json','a')
+f.write(json.dumps(data['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['returns']['latestPerf']))
 
 
