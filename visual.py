37fca57054e3f57cc025f087f7ebd2b8e6f20010
ssyuen <samuel.s.yuen@gmail.com>
2020-02-07 20:07:50 -0500

    output added

--- visual.py
+++ visual.py
@@ -5,5 +5,9 @@ import pandas as pd
 portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'BLK~25|AAPL~25|IXN~25|MALOX~25'})
 
 
+f = open('out.json','a')
+f.write(json.dumps(portfolioAnalysisRequest.json()))
+
+
 
 
