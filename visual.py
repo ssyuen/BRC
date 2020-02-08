b556881f2cb24f0a78e8dadb349a35d4882c8ce8
ssyuen <samuel.s.yuen@gmail.com>
2020-02-07 20:06:16 -0500

    visual.py added

---
+++ visual.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import pandas as pd
+
+portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis", params={'positions' : 'BLK~25|AAPL~25|IXN~25|MALOX~25'})
+
+
+
+
