6c09590fa95c65c783aeebe74ec33dc7faabb90c
ssyuen <samuel.s.yuen@gmail.com>
2020-02-11 23:06:52 -0500

    Cleaned up code

--- index.py
+++ index.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS, cross_origin 
+from flask_cors import CORS
 from data import call
 app = Flask(__name__)
 CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-#app.config['CORS_HEADERS'] = 'Content-Type'e
-
-#@cross_origin(origin='localhost',headers='Content- Type','Authorization'])
 
 @app.route("/")
 def landing_page():
@@ -19,13 +15,9 @@ def landing_page():
 @app.route("/user-api/",methods=['GET'])
 def user_api():
   args = request.args['ticks']
-
-  print(args, 'LOOK HERE')
   return jsonify(call(str(args)))
 
 @app.route('/blk-api/',methods=['GET'])
 def blk_api():
   args = request.args['ticks']
-  
-  print(args, 'LOOK HERE')
   return jsonify(call(str(args)))
